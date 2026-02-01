ef2b7f3ecc15531ce7c5d1aa497f51dcd2ba994e
Kallam Vishnu Vardhan Reddy <127661438+vishnukallam@users.noreply.github.com>
2026-02-01 17:24:14 +0000

    javascript

---
+++ index.js
@@ -0,0 +1,31 @@
+// // Variables.
+// let x = 123;
+// console.log("The value of X is ", x);
+// console.log(typeof x);
+// y = 124.4
+// console.log(typeof y)
+// let name = 'Vishnu';
+// console.log(typeof name);
+// console.log(`The type of x is ${typeof x}`);
+// let online = true;
+// console.log(`The status is ${online}`);
+
+// // Arithmetic operators
+// /* 
+// The arithmetic operators are +, -, /, *, **, and %
+// */ 
+// // console.log(++x);
+// y++
+// console.log(y)
+
+// // input from user by using window.prompt
+// let username = window.prompt("What is your username?");
+// console.log(`musername`);
+// input form user by using html element
+let username;
+document.getElementById("submit").onclick = function(){
+    username = document.getElementById("text").value;
+    console.log(username);
+};
+
+
