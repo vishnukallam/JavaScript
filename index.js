abe4957b43c1c75bfdc983486f1ae33d013932c3
Kallam Vishnu Vardhan Reddy <127661438+vishnukallam@users.noreply.github.com>
2026-02-02 11:08:07 +0000

    typecasting

--- index.js
+++ index.js
@@ -22,10 +22,38 @@
 // let username = window.prompt("What is your username?");
 // console.log(`musername`);
 // input form user by using html element
-let username;
-document.getElementById("submit").onclick = function(){
-    username = document.getElementById("text").value;
-    console.log(username);
-};
+// let username;
+// document.getElementById("submit").onclick = function(){
+//     username = document.getElementById("text").value;
+//     document.getElementById("h1text").innerText = `Welcome ${username}`;
+// };
 
 
+// Type conversion
+// let age
+// age = window.prompt("Enter your age: ");
+// age = age + 10; 
+// console.log("Your age after 10 years will be ", age);
+// Here the variable age has string type value. 
+// When add 10 to the age the string treated as string and it is concatenated to given input.
+
+//Know wenahve to convert string to number
+// age = Number(age);
+// age = age + 10;
+// console.log("Your age after 10 years will be ", age);
+// console.log(typeof age);
+
+// age = String(age);
+// console.log(typeof age);
+
+
+let X = "summer time"
+let y = "summer time"
+let z = "summer time"
+
+X = Boolean(X)
+y = Boolean(y)
+z = Boolean(z)
+console.log(X, typeof X)
+console.log(y, typeof y)
+console.log(z, typeof z)        
