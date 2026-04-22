101e3c2e2fd075169ae28fa6534a893320d2d331
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2026-04-22 16:51:56 +0530

    random

--- C#/Program.cs
+++ C#/Program.cs
@@ -10,25 +10,11 @@ namespace CSharp
             // Console.Write("Press any key to continue...");
             // Console.ReadKey();
             // Console.WriteLine("\aGoodbye!");
-            Console.Write("Enter a number:");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            Random random = new Random();
+            int num = random.Next(1, 101);
+            for (int i = 0; i <= 10; i++)
             {
-
-                if (i == 10)
-                {
-                    Console.WriteLine("Hey 10, you don't have access");
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
-
-            }
-            while (n > 0)
-            {
-                Console.WriteLine("This is an infinite loop");
+                Console.WriteLine(num);
             }
 
         }
