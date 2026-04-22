f261f814dfd54b5b6da510c2211e423f0130cf81
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2026-04-22 17:30:06 +0530

    for

--- C#/StringMethods.cs
+++ C#/StringMethods.cs
@@ -6,13 +6,13 @@ namespace CSharp
         static void Main(string[] args)
         {
             string myName = "Zoro";
-            Console.WriteLine(myName.Length);
-            Console.WriteLine(myName.ToLower());
-            Console.WriteLine(myName.ToArray());
-            Console.WriteLine(myName.Substring(0, 3));
-            Console.WriteLine(myName.Contains("or"));
-            Console.WriteLine(myName.StartsWith("Zo"));
-            Console.WriteLine(myName.EndsWith("ro"));
+            Console.WriteLine("Length of the String: " + myName.Length);
+            Console.WriteLine("String in lower case: " + myName.ToLower());
+            Console.WriteLine("String as character array: " + string.Join(", ", myName.ToArray()));
+            Console.WriteLine("Substring (0, 3): " + myName.Substring(0, 3));
+            Console.WriteLine("Contains 'or': " + myName.Contains("or"));
+            Console.WriteLine("Starts with 'Zo': " + myName.StartsWith("Zo"));
+            Console.WriteLine("Ends with 'ro': " + myName.EndsWith("ro"));
             Console.WriteLine(myName + " is a character in One Piece.");
             Console.WriteLine(myName[0]);
             Console.WriteLine(myName.IndexOf("o"));
@@ -20,6 +20,14 @@ namespace CSharp
             Console.WriteLine(myName.Replace("o", "a"));
             Console.WriteLine(myName.TrimEnd('o'));
 
+            string number = "9123-526-526";
+            Console.WriteLine(number.Replace("-", " "));
+
+            for (int i = 0; i < myName.Length; i++)
+            {
+                Console.Write(myName[i]);
+            }
         }
+
     }
 }
